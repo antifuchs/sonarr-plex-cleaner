5217c72fd93840d3b742ae39d643fbbe11bc88f0
Andreas Fuchs <asf@boinkor.net>
2019-07-22 08:36:10 +0200

    Docs for plex.rs

--- src/services.rs
+++ src/services.rs
@@ -1,2 +1,7 @@
+//! Accessing external APIs.
+//!
+//! In this module are all the types & methods we need to run the
+//! cleaner against media indexers & "watched" state keepers.
+
 pub mod plex;
 pub mod sonarr;
